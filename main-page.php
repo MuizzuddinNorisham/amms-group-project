62905e0b5a3da2ff4ee11d67cc8e235395c6defb
asyrfhqkmi <asyrafhaqimi07@gmail.com>
2025-07-09 14:47:38 +0800

    update all

--- main-page.php
+++ main-page.php
@@ -126,17 +126,6 @@ session_start();
                 <div class="box">
                     <div class="image">
                         <img src="prod2.jpg" alt="">
-                        <div class="icons">
-                            <a href="#" class="fas fa-heart"></a>
-                            <a href="#" 
-                            class="cart-btn" 
-                            data-name="rose gold mirror black design" 
-                            data-price="RM60.00" 
-                            data-image="prod2.jpg">
-                            Add to cart
-                            </a>
-                            <a href="#" class="fa-solid fa-circle-info"></a>
-                        </div>
                     </div>
                     <div class="content">
                         <h3>rose gold mirror black design</h3>
@@ -147,16 +136,6 @@ session_start();
                 <div class="box">
                     <div class="image">
                         <img src="prod3.jpg" alt="">
-                        <div class="icons">
-                            <a href="#" class="fas fa-heart"></a>
-                            <a href="#" 
-                            class="cart-btn" 
-                            data-name="gold line engraved design" 
-                            data-price="RM60.00" 
-                            data-image="prod3.jpg">
-                            Add to cart
-                            </a>
-                            <a href="#" class="fa-solid fa-circle-info"></a>
                         </div>
                     </div>
                     <div class="content">
@@ -168,16 +147,6 @@ session_start();
                 <div class="box">
                     <div class="image">
                         <img src="prod4.jpg" alt="">
-                        <div class="icons">
-                            <a href="#" class="fas fa-heart"></a>
-                            <a href="#" 
-                            class="cart-btn" 
-                            data-name="clear lettering black background" 
-                            data-price="RM45.00" 
-                            data-image="prod4.jpg">
-                            Add to cart
-                            </a>
-                            <a href="#" class="fa-solid fa-circle-info"></a>
                         </div>
                     </div>
                     <div class="content">
@@ -189,16 +158,6 @@ session_start();
                 <div class="box">
                     <div class="image">
                         <img src="prod5.jpg" alt="">
-                        <div class="icons">
-                            <a href="#" class="fas fa-heart"></a>
-                            <a href="#" 
-                            class="cart-btn" 
-                            data-name="clear color infill" 
-                            data-price="RM60.00" 
-                            data-image="prod5.jpg">
-                            Add to cart
-                            </a>
-                            <a href="#" class="fa-solid fa-circle-info"></a>
                         </div>
                     </div>
                     <div class="content">
@@ -210,16 +169,6 @@ session_start();
                 <div class="box">
                     <div class="image">
                         <img src="prod6.jpg" alt="">
-                        <div class="icons">
-                            <a href="#" class="fas fa-heart"></a>
-                            <a href="#" 
-                            class="cart-btn" 
-                            data-name="rose gold mirror clear design" 
-                            data-price="RM60.00" 
-                            data-image="prod6.jpg">
-                            Add to cart
-                            </a>
-                            <a href="#" class="fa-solid fa-circle-info"></a>
                         </div>
                     </div>
                     <div class="content">
